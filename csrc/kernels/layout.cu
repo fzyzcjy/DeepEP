b0ba0ea5bd9f1a2e9241b496d2b5f5bc63510089
fzyzcjy <ch271828n@outlook.com>
2025-06-17 17:56:11 +0800

    Revert "more"
    This reverts commit 0613b1ff137d223afcf6b369a3ce5893729bbd82.

--- csrc/kernels/layout.cu
+++ csrc/kernels/layout.cu
@@ -121,7 +121,7 @@ void get_dispatch_layout(const int64_t* topk_idx,
                          int* num_tokens_per_expert, bool* is_token_in_rank,
                          int num_tokens, int num_topk, int num_ranks, int num_experts,
                          cudaStream_t stream) {
-    constexpr int kNumThreads = 256, kNumExpertsPerSM = 32, kNumRanksPerSM = NUM_MAX_NVL_PEERS;
+    constexpr int kNumThreads = 256, kNumExpertsPerSM = 32, kNumRanksPerSM = 8;
     int num_sms = ((num_experts + kNumExpertsPerSM - 1) / kNumExpertsPerSM) + (num_ranks + kNumRanksPerSM - 1) / kNumRanksPerSM;
     EP_STATIC_ASSERT(kNumExpertsPerSM % NUM_MAX_NVL_PEERS == 0, "Invalid number of experts per SM");
 
