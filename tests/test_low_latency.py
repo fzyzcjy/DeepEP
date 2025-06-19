7bce77adf6bcd7ab47e2446904a41efff8a77938
fzyzcjy <ch271828n@outlook.com>
2025-06-19 13:44:38 +0800

    cp

--- tests/test_low_latency.py
+++ tests/test_low_latency.py
@@ -1,3 +1,5 @@
+import json
+import os
 import random
 import torch
 import torch.distributed as dist
@@ -30,7 +32,7 @@ def test_main(num_tokens: int, hidden: int, num_experts: int, num_topk: int,
         topk_idx[random.randint(0, num_tokens - 1), random.randint(0, num_topk - 1)] = -1
 
     # Check dispatch correctness
-    do_check = True
+    do_check = bool(int(os.environ.get("DEEPEP_HACK_DO_CHECK", "1")))
     hash_value, num_times = 0, 0
     for return_recv_hook in (False, True):
         for dispatch_use_fp8 in (False, True):
@@ -152,6 +154,21 @@ def test_main(num_tokens: int, hidden: int, num_experts: int, num_topk: int,
         else:
             print(f'[rank {rank}] Dispatch send/recv time: {dispatch_t * 2 * 1e6:.2f} us | '
                   f'Combine send/recv time: {combine_t * 2 * 1e6:.2f} us', flush=True)
+        print('MAIN_OUTPUT=' + json.dumps(dict(
+            rank=rank,
+            num_tokens=num_tokens,
+            hidden=hidden,
+            num_experts=num_experts,
+            num_topk=num_topk,
+            num_ranks=num_ranks,
+
+            return_recv_hook=return_recv_hook,
+
+            num_dispatch_comm_bytes=num_dispatch_comm_bytes,
+            num_combine_comm_bytes=num_combine_comm_bytes,
+            dispatch_t=dispatch_t,
+            combine_t=combine_t,
+        )))
 
     return hash_value
 
@@ -159,13 +176,20 @@ def test_main(num_tokens: int, hidden: int, num_experts: int, num_topk: int,
 # noinspection PyUnboundLocalVariable
 def test_loop(local_rank: int, num_local_ranks: int):
     rank, num_ranks, group = init_dist(local_rank, num_local_ranks)
-    num_tokens, hidden, num_topk, num_experts = 128, 7168, 8, 288
+    # num_tokens, hidden, num_topk, num_experts = 128, 7168, 8, 288
+    num_tokens = int(os.environ.get("DEEPEP_TEST_NUM_TOKENS", "128"))
+    hidden = int(os.environ.get("DEEPEP_TEST_HIDDEN", "7168"))
+    num_topk = int(os.environ.get("DEEPEP_TEST_NUM_TOPK", "8"))
+    num_experts = int(os.environ.get("DEEPEP_TEST_NUM_EXPERTS", "288"))
+    print(f"{num_tokens=} {hidden=} {num_topk=} {num_experts=}")
 
     num_rdma_bytes = deep_ep.Buffer.get_low_latency_rdma_size_hint(num_tokens, hidden, num_ranks, num_experts)
     if local_rank == 0:
         print(f'Allocating buffer size: {num_rdma_bytes / 1e6} MB ...', flush=True)
     buffer = deep_ep.Buffer(group, num_rdma_bytes=num_rdma_bytes, low_latency_mode=True,
-                            num_qps_per_rank=num_experts // num_ranks)
+                            num_qps_per_rank=num_experts // num_ranks,
+                            allow_mnnvl=bool(int(os.environ.get("DEEPEP_TEST_ALLOW_MNNVL", "0"))))
+    buffer.get_low_latency_usage_flag()
     test_main(num_tokens, hidden, num_experts, num_topk, rank, num_ranks, group, buffer, seed=1)
 
     do_pressure_test = False
@@ -183,5 +207,6 @@ def test_loop(local_rank: int, num_local_ranks: int):
 
 if __name__ == '__main__':
     # TODO: you may modify NUMA binding for less CPU overhead
-    num_processes = 8
+    # num_processes = 8
+    num_processes = int(os.environ.get("DEEPEP_TEST_NUM_PROCESSES", "8"))
     torch.multiprocessing.spawn(test_loop, args=(num_processes,), nprocs=num_processes)
