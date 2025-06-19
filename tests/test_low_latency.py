1c15cb0f0e1e15526bbefcad4ac23640d0f9094e
fzyzcjy <ch271828n@outlook.com>
2025-06-19 13:46:54 +0800

    fix

--- tests/test_low_latency.py
+++ tests/test_low_latency.py
@@ -189,7 +189,6 @@ def test_loop(local_rank: int, num_local_ranks: int):
     buffer = deep_ep.Buffer(group, num_rdma_bytes=num_rdma_bytes, low_latency_mode=True,
                             num_qps_per_rank=num_experts // num_ranks,
                             allow_mnnvl=bool(int(os.environ.get("DEEPEP_TEST_ALLOW_MNNVL", "0"))))
-    buffer.get_low_latency_usage_flag()
     test_main(num_tokens, hidden, num_experts, num_topk, rank, num_ranks, group, buffer, seed=1)
 
     do_pressure_test = False
