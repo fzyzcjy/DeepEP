db53053fe8ef86e644914436c71f23ed81ec1ffa
fzyzcjy <ch271828n@outlook.com>
2025-06-21 07:42:30 +0800

    more

--- tests/utils.py
+++ tests/utils.py
@@ -152,7 +152,8 @@ class suppress_stdout_stderr:
 
 
 def bench_kineto(fn, kernel_names, num_tests: int = 30, suppress_kineto_output: bool = False,
-                 trace_path: Optional[str] = None, barrier_comm_profiling: bool = False):
+                 trace_path: Optional[str] = None, barrier_comm_profiling: bool = False,
+                 duplicate_name_period: Optional[int] = None):
     # Profile
     suppress = suppress_stdout_stderr if suppress_kineto_output else empty_suppress
     with suppress():
