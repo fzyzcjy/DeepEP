7fbd0f002102f07e384e0eb2916c21c8232a5202
fzyzcjy <ch271828n@outlook.com>
2025-06-21 21:06:55 +0800

    more

--- setup.py
+++ setup.py
@@ -22,6 +22,9 @@ if __name__ == '__main__':
     # NOTE MODIFIED
     extra_link_args = ['-lcuda']
 
+    # NOTE MODIFIED
+    nvcc_flags += ['-lineinfo']
+
     # NVSHMEM flags
     if disable_nvshmem:
         cxx_flags.append('-DDISABLE_NVSHMEM')
