f1b2d1b6a9acbd3653e3d15d279e9956da546754
fzyzcjy <ch271828n@outlook.com>
2025-06-23 11:51:11 +0800

    proto

--- csrc/kernels/internode_ll.cu
+++ csrc/kernels/internode_ll.cu
@@ -452,7 +452,17 @@ combine(void* combined_x,
             const auto rdma_send_x_vec_row = reinterpret_cast<uint8_t*>(rdma_send_type_row);
 
             // Copy directly to local rank, or copy to buffer and issue RDMA
-            auto src_idx = __ldg(local_src_info + token_idx);
+
+            // NOTE HACK
+//             auto src_idx = __ldg(local_src_info + token_idx);
+            auto src_idx = token_idx - offset;
+            // TODO
+            // TODO
+            // TODO remove
+            // TODO
+            // TODO
+            EP_DEVICE_ASSERT(src_idx < num_max_dispatch_tokens_per_rank);
+
             const auto buf_ptr = reinterpret_cast<int64_t>(rdma_send_x_vec_row);
             const auto dst_ptr = reinterpret_cast<uint64_t>(rdma_recv_x) + (global_expert_idx * num_max_dispatch_tokens_per_rank + src_idx) * num_bytes_per_slot;
             const auto dst_p2p_ptr = nvshmemi_get_p2p_ptr(dst_ptr, rank, dst_rank);
