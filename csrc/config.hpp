683bd7e76233030a2a276207dda1d241530d0fe2
fzyzcjy <ch271828n@outlook.com>
2025-08-21 20:00:32 +0800

    more

--- csrc/config.hpp
+++ csrc/config.hpp
@@ -156,10 +156,12 @@ struct LowLatencyLayout {
         EP_HOST_ASSERT(recv_buffer_bytes % sizeof(int4) == 0);
         total_bytes += recv_buffer_bytes * 2;
 
+        size_t hack_extra_signaling_buffer_bytes = num_experts * num_ranks * sizeof(uint16_t);
+
         // Symmetric signaling buffers
         size_t dispatch_recv_count_buffer_bytes = num_experts * sizeof(int);
         size_t combine_recv_flag_buffer_bytes = dispatch_recv_count_buffer_bytes;
-        size_t signaling_buffer_bytes = std::max(dispatch_recv_count_buffer_bytes, combine_recv_flag_buffer_bytes);
+        size_t signaling_buffer_bytes = std::max(dispatch_recv_count_buffer_bytes, combine_recv_flag_buffer_bytes) + hack_extra_signaling_buffer_bytes;
         size_t signaling_buffer_bytes_aligned = align<size_t>(signaling_buffer_bytes, 128);
         total_bytes += signaling_buffer_bytes_aligned * 2;
 
