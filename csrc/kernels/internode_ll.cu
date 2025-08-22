172ecd97566ca239611259e13de10c4c08135017
fzyzcjy <ch271828n@outlook.com>
2025-08-22 11:38:32 +0800

    combine_a use 288/9 sm

--- csrc/kernels/internode_ll.cu
+++ csrc/kernels/internode_ll.cu
@@ -929,7 +929,9 @@ void combine(void* combined_x,
              void* workspace, int num_device_sms,
              cudaStream_t stream, int phases, bool zero_copy) {
     constexpr int kNumMaxTopk = 9;
-    const int num_warp_groups = ceil_div(num_experts, num_device_sms);
+    const int num_warp_groups = ((phases & LOW_LATENCY_RECV_PHASE) == 0)
+        ? 9
+        : ceil_div(num_experts, num_device_sms);
     const int num_warps_per_group = 32 / num_warp_groups;
     const int num_recv_per_sm = ceil_div(num_combined_tokens, num_device_sms);
     EP_HOST_ASSERT(num_warp_groups > 0 and num_warps_per_group > 0 and num_recv_per_sm > 0);
